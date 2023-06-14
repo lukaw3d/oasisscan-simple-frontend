4927a52418cef0c50f38b08cd2f319ad21391350
lukaw3d <lukajeran@oasislabs.com>
2023-06-14 17:34:45 +0200

    Add orval to auto-generate react-query from Oasis Indexer OpenAPI

---
+++ src/oasis-indexer/orval.config.js
@@ -0,0 +1,16 @@
+/** @type {import('@orval/core').Config} */
+module.exports = {
+  indexer: {
+    input: {
+      // target: './v1.yaml',
+      // target: 'https://raw.githubusercontent.com/oasisprotocol/oasis-indexer/main/api/spec/v1.yaml',
+      // target: 'https://index-staging.oasislabs.com/v1/spec/v1.yaml',
+      target: 'https://index.oasislabs.com/v1/spec/v1.yaml',
+    },
+    output: {
+      target: './generated/api.ts',
+      client: 'react-query',
+      mode: 'single',
+    },
+  },
+}
