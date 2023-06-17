26d38a7ae7029235b329cabd615ccf9ba739296c
lukaw3d <lukajeran@oasislabs.com>
2023-06-17 19:29:43 +0200

    Buffer runtime issues

--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,7 @@ import './index.css'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { RouterProvider, createBrowserRouter } from 'react-router-dom';
 import { routes } from './pages/routes'
+import './polyfill'
 
 axios.defaults.baseURL = 'https://index.oasislabs.com/v1/'
 const queryClient = new QueryClient({
