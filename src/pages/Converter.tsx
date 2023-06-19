e84801d3e32d9f70f32315563a16e2cbe64a301f
lukaw3d <lukajeran@oasislabs.com>
2023-06-19 21:39:27 +0200

    Rename variables in account address converter

--- src/pages/Converter.tsx
+++ src/pages/Converter.tsx
@@ -4,37 +4,36 @@ import { getEvmBech32Address } from '../utils/getEvmBech32Address'
 import { getEthAccountAddress } from '../utils/getEthAccountAddress'
 
 export function Converter() {
-  const [fromEvm, setFromEvm] = useState("0xba504818fdd8d3dba2ef8fd9b4f4d5c71ad1d1d3")
-  const [fromEvmResult, setFromEvmResult] = useState("")
+  const [fromEvmAccount, setFromEvmAccount] = useState("0xba504818fdd8d3dba2ef8fd9b4f4d5c71ad1d1d3")
+  const [fromEvmAccountResult, setFromEvmAccountResult] = useState("")
   useEffect(() => {
     (async () => {
-      setFromEvmResult(await getEvmBech32Address(fromEvm))
+      setFromEvmAccountResult(await getEvmBech32Address(fromEvmAccount))
     })()
-  }, [fromEvm])
+  }, [fromEvmAccount])
 
-  const [fromOasis, setFromOasis] = useState("oasis1qrvha284gfztn7wwq6z50c86ceu28jp7csqhpx9t")
-  const emeraldAccount = useGetRuntimeAccountsAddress('emerald', fromOasis)
-  const sapphireAccount = useGetRuntimeAccountsAddress('sapphire', fromOasis)
-
-  const fromOasisResult = emeraldAccount.isLoading || sapphireAccount.isLoading
+  const [fromOasisAccount, setFromOasisAccount] = useState("oasis1qrvha284gfztn7wwq6z50c86ceu28jp7csqhpx9t")
+  const emeraldAccount = useGetRuntimeAccountsAddress('emerald', fromOasisAccount)
+  const sapphireAccount = useGetRuntimeAccountsAddress('sapphire', fromOasisAccount)
+  const fromOasisAccountResult = emeraldAccount.isLoading || sapphireAccount.isLoading
     ? 'loading'
-    : getEthAccountAddress(emeraldAccount.data?.data.address_preimage) ?? getEthAccountAddress(sapphireAccount.data?.data.address_preimage) ?? '?'
+    : getEthAccountAddress(emeraldAccount.data?.data.address_preimage) ?? getEthAccountAddress(sapphireAccount.data?.data.address_preimage) ?? 'not found'
 
   return (
     <div>
       <section>
-        <h2>Convert EVM address to oasis1 (using addressToBech32(fromData(fromHex(evmAddress))))</h2>
-        <input type="text" value={fromEvm} onChange={e => setFromEvm(e.target.value)} size={70} />
+        <h2>Convert EVM account to oasis1 (using addressToBech32(fromData(fromHex(evmAddress))))</h2>
+        <input type="text" value={fromEvmAccount} onChange={e => setFromEvmAccount(e.target.value)} size={80} />
         <br />
-        Result: {fromEvmResult}
+        Result: {fromEvmAccountResult}
       </section>
       <br />
       <br />
       <section>
-        <h2>Convert oasis1 address to EVM (using https://index.oasislabs.com/v1/emerald/accounts/oasis1.. address_preimage)</h2>
-        <input type="text" value={fromOasis} onChange={e => setFromOasis(e.target.value)} size={70} />
+        <h2>Convert oasis1 account to EVM (using https://index.oasislabs.com/v1/emerald/accounts/oasis1.. address_preimage)</h2>
+        <input type="text" value={fromOasisAccount} onChange={e => setFromOasisAccount(e.target.value)} size={80} />
         <br />
-        Result: {fromOasisResult}
+        Result: {fromOasisAccountResult}
       </section>
       <br />
       <br />
