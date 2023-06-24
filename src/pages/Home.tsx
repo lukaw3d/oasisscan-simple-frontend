ad85d103fe791e6d14796cf46f8d3499bc4cd5cb
lukaw3d <lukajeran@oasislabs.com>
2023-06-24 12:20:13 +0200

    Prevent layout shifting on homepage

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -5,31 +5,43 @@ import { useGetRuntimeStatus, useGetStatus } from '../oasis-indexer/generated/ap
 export function Home() {
   return (
     <>
-      <h2>Consensus status</h2>
-      <DisplayData result={useGetStatus()}></DisplayData>
+      <h2>Consensus</h2>
       <Link to="/consensus/blocks?limit=100&offset=0">Blocks</Link>, <Link to="/consensus/blocks?limit=1000&to=10000000">Blocks&lt;10000000</Link><br />
       <Link to="/consensus/transactions?limit=100&offset=0">Transactions</Link><br />
       <Link to="/consensus/events?limit=100&offset=0">Events</Link><br />
       <Link to="/consensus/accounts/oasis1qzp86dykm9nfqr83es07c324u64f6dgsdvk0fapj">Accounts</Link><br />
+      Status:
+      <div style={{minHeight: '5em'}}>
+        <DisplayData result={useGetStatus()}></DisplayData>
+      </div>
 
-      <h2>Emerald status</h2>
-      <DisplayData result={useGetRuntimeStatus('emerald')}></DisplayData>
+      <h2>Emerald</h2>
       <Link to="/emerald/blocks?limit=100&offset=0">Blocks</Link>, <Link to="/emerald/blocks?limit=1000&to=1000">Blocks&lt;1000</Link><br />
       <Link to="/emerald/transactions?limit=100&offset=0">Transactions</Link><br />
       <Link to="/emerald/events?limit=100&offset=0">Events</Link><br />
       <Link to="/emerald/accounts/oasis1qrvha284gfztn7wwq6z50c86ceu28jp7csqhpx9t">Accounts</Link><br />
       <Link to="/emerald/tokens?limit=100&offset=0">Tokens</Link><br />
+      Status:
+      <div style={{minHeight: '5em'}}>
+        <DisplayData result={useGetRuntimeStatus('emerald')}></DisplayData>
+      </div>
 
-      <h2>Sapphire status</h2>
-      <DisplayData result={useGetRuntimeStatus('sapphire')}></DisplayData>
+      <h2>Sapphire</h2>
       <Link to="/sapphire/blocks?limit=100&offset=0">Blocks</Link>, <Link to="/sapphire/blocks?limit=1000&to=1000">Blocks&lt;1000</Link><br />
       <Link to="/sapphire/transactions?limit=100&offset=0">Transactions</Link><br />
       <Link to="/sapphire/events?limit=100&offset=0">Events</Link><br />
       <Link to="/sapphire/accounts/oasis1qpupfu7e2n6pkezeaw0yhj8mcem8anj64ytrayne">Accounts</Link><br />
       <Link to="/sapphire/tokens?limit=100&offset=0">Tokens</Link><br />
+      Status:
+      <div style={{minHeight: '5em'}}>
+        <DisplayData result={useGetRuntimeStatus('sapphire')}></DisplayData>
+      </div>
 
-      <h2>Cipher status</h2>
-      <DisplayData result={useGetRuntimeStatus('cipher')}></DisplayData>
+      <h2>Cipher</h2>
+      Status:
+      <div style={{minHeight: '5em'}}>
+        <DisplayData result={useGetRuntimeStatus('cipher')}></DisplayData>
+      </div>
 
       <br />
       <h2>Misc</h2>
