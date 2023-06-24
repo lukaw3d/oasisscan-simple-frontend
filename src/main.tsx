4093be9e4de9d9c158a1f8adbefe1f04fb667543
lukaw3d <lukajeran@oasislabs.com>
2023-06-24 02:03:11 +0200

    Use hash router to fix github pages

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client'
 import axios from 'axios'
 import './index.css'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
-import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import { RouterProvider, createHashRouter } from 'react-router-dom';
 import { routes } from './pages/routes'
 import './utils/polyfill'
 
@@ -19,7 +19,7 @@ const queryClient = new QueryClient({
   },
 })
 
-export const router = createBrowserRouter(routes)
+export const router = createHashRouter(routes)
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
