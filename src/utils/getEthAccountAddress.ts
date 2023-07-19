0e00cbe4ad5f28aecd96d020336d400e9366f08d
lukaw3d <lukajeran@oasislabs.com>
2023-07-19 23:53:08 +0200

    Show eth checksum addresses

--- src/utils/getEthAccountAddress.ts
+++ src/utils/getEthAccountAddress.ts
@@ -1,6 +1,7 @@
 import { AddressPreimage } from '../oasis-indexer/generated/api'
+import { getAddress as getChecksumAddress } from 'ethers/address'
 
 export function getEthAccountAddress(preimage: AddressPreimage | undefined): string | undefined {
   if (preimage?.context !== 'oasis-runtime-sdk/address: secp256k1eth' || !preimage.address_data) return undefined
-  return '0x' + Buffer.from(preimage.address_data, 'base64').toString('hex')
+  return getChecksumAddress('0x' + Buffer.from(preimage.address_data, 'base64').toString('hex'))
 }
